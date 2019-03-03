0e2e48e911b814dfb04efe4f7c43aa186eca6a7f
Avva_Carnet <bookmybook99@gmail.com>
2019-03-03 15:53:31 +0530

    React router setup with login and home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
 
-//import './App.css';
+const App = () => (
+	<div>
+		<Route path="/" exact component={HomePage} />
+		<Route path="/login" exact component={LoginPage} />
+	</div>
+);
 
-class App extends Component {
+/*class App extends Component {
 	render() {
 		return (
 			<div className="App">
-
+				<h1>BOOK MY BOOK</h1>
 			</div>
 		);
 	}
-}
+}*/
 
 export default App;
